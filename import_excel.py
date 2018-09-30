452a25eed690fd70a261ed05954caef86afd7277
Catherine Jeffries <catherinej@vt.edu>
2018-09-30 16:04:02 -0400

    Adding and updating a few scripts

---
+++ import_excel.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+file = '/home/catherinej/Downloads/IrmaMudThicknessComparisons.xlsx'
+
+data_original = pd.read_excel(file, sheet_name='Original Data')
+data_fake = pd.read_excel(file, sheet_name='Fake data')
+
+print(data_fake, data_original)
+
+
+import os
+import tarfile
+from six.moves import urllib
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+        tgz_path = os.path.join(housing_path, "housing.tgz")
+        urllib.request.urlretrieve(housing_url, tgz_path)
+        housing_tgz = tarfile.open(tgz_path)
+        housing_tgz.extractall(path=housing_path)
+        housing_tgz.close()
+fetch_housing_data(HOUSING_URL, HOUSING_PATH)
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+housing = load_housing_data()
+
+import numpy as np
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+#You can then use this function like this:
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
